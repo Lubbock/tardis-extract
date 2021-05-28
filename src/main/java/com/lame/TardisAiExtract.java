d11aaceba8ef51fcbadc78b52d131a146df7e13d
guoxj <guoxj@koal.com>
2021-05-28 11:57:04 +0800

    fix: 增加配置
    增加配置

--- src/main/java/com/lame/TardisAiExtract.java
+++ src/main/java/com/lame/TardisAiExtract.java
@@ -12,6 +12,7 @@ import com.lame.sbconstant.command.KoalExtractEntityCommand;
 
 public class TardisAiExtract extends AnAction {
     KoalExtractEntityCommand command = new KoalExtractEntityCommand();
+
     @Override
     public void actionPerformed(AnActionEvent e) {
         //获取当前在操作的工程上下文
@@ -22,11 +23,13 @@ public class TardisAiExtract extends AnAction {
         //获取当前类文件的路径
         String classPath = psiFile.getVirtualFile().getPath();
         try {
-      Context ctx = new Context();
-      ctx.aiDetect = true;
+            Context ctx = new Context();
+            ctx.aiDetect = true;
             command.execute(ctx, classPath);
+            Messages.showMessageDialog(project, "文件生成成功请刷新项目", "常量文件生成成功", Messages.getInformationIcon());
         } catch (Exception exception) {
             exception.printStackTrace();
+            Messages.showMessageDialog(project, "文件解析失败", "文件解析失败", Messages.getInformationIcon());
         }
 
 //        String title = "Hello World!";
